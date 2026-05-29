642be22fc5aa39948f524b3371311fa05bc84ad3
Nitishhpandey <nitishpandey3108@gmail.com>
2026-05-29 13:24:31 +0530

    first commit

---
+++ day2.js
@@ -0,0 +1,75 @@
+
+// console.log ("age is ",age)
+
+// variable
+
+// var age = 24;
+// age  = 35;
+
+// age ='Nitish'
+
+// global and scoping  || hoisiting // function scope
+
+// this whole programming pattern is wrong its not a good pattern
+
+// js team deprecate this var
+
+// two new terms introduced let and const
+
+// age =65;   // here we can not acess age before initailisation 
+// console.log("age is ", age);
+
+// let age = 24;
+// console.log("age is ", age);
+
+// age = 45           // we can reassign the age
+// console.log ("age is ",age);
+
+
+
+// cosnt 
+// you can't change the value of that particular 
+
+// const name  = 'nitish'; // here also temporal dead zone is created // hoisting is done.
+
+
+// function concept
+// function is reusable set of instruction || a block of code desiged ti perform the specific task
+
+
+// function sayHello (){
+//  console.log('bye');
+// }
+// sayHello();
+
+// Function ke andr jo input lete hin usko parameters bolte hin 
+
+// function greetUser(x,y,z){
+//     console.log (`hey ${x} ${y} ${z}`);  // use of bactick keyword allows us to use the multiple varibale in a single line
+// }
+
+// greetUser('nitish', 'pandey', '.');
+
+// we can accept more than one parameter in function
+
+// function add(num1, num2){
+// console.log(`reusult is ${num1+num2}`); // here the problem is we have directly print the value ieadally we have to store the value in another variable for further use.
+
+// const result = num1 + num2;
+// return result;
+
+// }
+
+//  const r = add (2,5);
+//   for(let i =1; i<=r; i++) {
+//     console.log(`value of i is ${i}`);
+//   }
+
+// we can't return a single function multiple times once return statement is written nothing to be write after that.
+
+
+// a function can contain a functoin inside it 
+
+// a funciton can return boolean null ......
+
+    
