09f2169b5be4585c23c437128014173dae4fcd45
Nitishhpandey <nitishpandey3108@gmail.com>
2026-05-30 07:53:24 +0530

    learn arrary concept

---
+++ array.js
@@ -0,0 +1,25 @@
+// array
+const fruits = ['apple', 'cheeku', 'addu', 'santra']
+
+// array working 
+fruits.push('kiwi') // push add new element in the last of the array
+console.log(fruits)
+
+console.log(fruits.length); //5 because we have push the kiwi or update the array
+
+console.log(fruits.includes('addu')) // checks whether addu is present in the array or not.
+
+const firstElement = fruits.shift() //removes the first elements
+
+console.log (fruits);
+console.log(firstElement)
+
+// ques and stack implementation using array
+
+
+// high order function 
+
+function merapyaraFunction(){
+    return 2+3+num1
+}
+merapyaraFunction(5)
