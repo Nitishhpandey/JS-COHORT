ea7cd070e48f623fd5776aaafa85566b9ffd5073
Nitishhpandey <nitishpandey3108@gmail.com>
2026-06-02 13:01:47 +0530

    js essential concept

---
+++ 01_console.js
@@ -0,0 +1,9 @@
+const clue1 ="muddy footprint found near the window"
+const clue2 =" broken glass on the table"
+
+console.log ("clue found:", clue1)
+console.log ("clue found",clue2)
+
+const suspectName = "nitish"
+const suspectAge = 20;
+console.log ("Suspect:",suspectName,"|Age: ")
